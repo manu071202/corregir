119cdb772eb55832ff88fa391410799fba2f2bfe
manuel ibanez <manuelibanezastorga@gmail.com>
2022-08-08 16:17:11 -0300

    arreglamos el codigo, primera parte

--- js/app.js
+++ js/app.js
@@ -8,43 +8,40 @@ function cambiarTitulo(){
 }
 
 function verMas(){
-
-    let botonVermas = document.getElementById("btnVermas")
-    console.log(botonVermas)
+    let botonVerMas =document.getElementById("btnVerMas");
     let sectionPadre = document.getElementsByClassName("container")
 
-    if(botonVermas.innerHTML === "ver mas.."){
+    if(botonVerMas.innerHTML === "ver mas.."){
 
-        //crear el parrafo ,agregar o insertar este nuevo elemento
-        //crear el parrafo primera forma
-//    let parrafoNuevo = document.createElement("p");
-    //preparar el elemento
-  //  parrafoNuevo.innerHTML ="Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis sequi illum, error atque vitae, iure cumque molestias suscipit dolores vel ipsam dolorum dolore. Quisquam voluptatum, illum, repellendus est voluptates, soluta nostrum non inventore odio aliquam perferendis excepturi voluptas quam magni ipsam. Corrupti sunt, ut vel voluptas exercitationem velit natus architecto."
-    //parrafoNuevo.className = "lead"
-   // console.log(parrafoNuevo)
-
+        // crear el parrafo primera forma
+        let parrafoNuevo = document.createElement("p"); //<p></p>
+        //  preparar el elemento
+        parrafoNuevo.innerHTML ="Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis sequi illum, error atque vitae, iure cumque molestias suscipit dolores vel ipsam dolorum dolore. Quisquam voluptatum, illum, repellendus est voluptates, soluta nostrum non inventore odio aliquam perferendis excepturi voluptas quam magni ipsam. Corrupti sunt, ut vel voluptas exercitationem velit natus architecto."
+    parrafoNuevo.className = "lead"
+    console.log(parrafoNuevo)
+    
     //traer el elemento padre
-   // console.log(sectionPadre[1])
+    console.log(sectionPadre[1]);
     //agregar o insertar en dom
-   // sectionPadre[1].appendChild(parrafoNuevo) //agreaga el nodo hijo al final
-    //sectionPadre[1].appendChild(parrafoNuevo) //agregar el nodo hijo al principio
-
-    //SEGUNDA FOTMA PARA CREAR UN PARRAFO
-    let parrafoNuevo = `<p class= "lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur soluta in sit, reprehenderit similique minus vitae illo reiciendis ex! Maxime labore fugit qui unde, quae nobis vel ipsum! Impedit, esse.</p>`
-
-    sectionPadre[1].innerHTML = parrafoNuevo
-
-
+    //sectionPadre[1].appendChild(parrafoNuevo) //agreaga el nodo hijo al final
+    //sectionPadre[1].prepend(parrafoNuevo) //agregar el nodo hijo al principio
+    sectionPadre[1].insertBefore(parrafoNuevo, botonVerMas);//agregar el nodo hijo
     
     //cambiar el texto del boton
-    botonVermas.className = "btn btn-outline-danger"
-    botonVermas.innerHTML = "ocultar"
+    botonVerMas.className= "btn btn-outline-danger"
+    botonVerMas.innerHTML= "ocultar"
 }else{
-    console.log("aqui quiero borrar u ocultar el parrafo")
-    console.log(sectionPadre[1].childrem[2])
-    sectionPadre[1].insertBefore(sectionPadre[1].childrem[2])
+    console.log("quiero ocultar")
+    //eliminar el parrafo creado
+    console.log(sectionPadre[1].children[2])
+    sectionPadre[1].removeChild(sectionPadre[1].children[2])
+    //resetear el boton
+    botonVerMas.innerHTML = "ver mas.."
+    botonVerMas.className = "btn btn-outline-primary";
+
+
 }
 
-
-sectionPadre[1].insertBefore(parrafoNuevo, document.getElementById("btnVermas"))//agregar el nodo hijo
+//NOTA PARA ACCEDER A LOS VALORES DENTRO DE UN IMPUT , NO USAR INNERHTML SINO VALUE
+    console.log(document.getElementById("buscador").value)
 }
