f8b58f97fd01c5601a611a189544b11309006178
manuel ibanez <manuelibanezastorga@gmail.com>
2022-08-08 10:38:35 -0300

    corregir  el boton

--- js/app.js
+++ js/app.js
@@ -9,20 +9,42 @@ function cambiarTitulo(){
 
 function verMas(){
 
-    console.log("prueba ver mas")
-    //crear el parrafo ,agregar o insertar este nuevo elemento
-    //crear el parrafo primera forma
-    let parrafoNuevo = document.createElement("p");
+    let botonVermas = document.getElementById("btnVermas")
+    console.log(botonVermas)
+    let sectionPadre = document.getElementsByClassName("container")
+
+    if(botonVermas.innerHTML === "ver mas.."){
+
+        //crear el parrafo ,agregar o insertar este nuevo elemento
+        //crear el parrafo primera forma
+//    let parrafoNuevo = document.createElement("p");
     //preparar el elemento
-    parrafoNuevo.innerHTML ="Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis sequi illum, error atque vitae, iure cumque molestias suscipit dolores vel ipsam dolorum dolore. Quisquam voluptatum, illum, repellendus est voluptates, soluta nostrum non inventore odio aliquam perferendis excepturi voluptas quam magni ipsam. Corrupti sunt, ut vel voluptas exercitationem velit natus architecto."
-    parrafoNuevo.className = "lead"
-    console.log(parrafoNuevo)
+  //  parrafoNuevo.innerHTML ="Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis sequi illum, error atque vitae, iure cumque molestias suscipit dolores vel ipsam dolorum dolore. Quisquam voluptatum, illum, repellendus est voluptates, soluta nostrum non inventore odio aliquam perferendis excepturi voluptas quam magni ipsam. Corrupti sunt, ut vel voluptas exercitationem velit natus architecto."
+    //parrafoNuevo.className = "lead"
+   // console.log(parrafoNuevo)
 
     //traer el elemento padre
-    let sectionPadre = document.getElementsByClassName("container")
-    console.log(sectionPadre[1])
+   // console.log(sectionPadre[1])
     //agregar o insertar en dom
    // sectionPadre[1].appendChild(parrafoNuevo) //agreaga el nodo hijo al final
     //sectionPadre[1].appendChild(parrafoNuevo) //agregar el nodo hijo al principio
-    sectionPadre[1].insertBefore(parrafoNuevo, document.getElementById("btnVermas"))//agregar el nodo hijo
+
+    //SEGUNDA FOTMA PARA CREAR UN PARRAFO
+    let parrafoNuevo = `<p class= "lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur soluta in sit, reprehenderit similique minus vitae illo reiciendis ex! Maxime labore fugit qui unde, quae nobis vel ipsum! Impedit, esse.</p>`
+
+    sectionPadre[1].innerHTML = parrafoNuevo
+
+
+    
+    //cambiar el texto del boton
+    botonVermas.className = "btn btn-outline-danger"
+    botonVermas.innerHTML = "ocultar"
+}else{
+    console.log("aqui quiero borrar u ocultar el parrafo")
+    console.log(sectionPadre[1].childrem[2])
+    sectionPadre[1].insertBefore(sectionPadre[1].childrem[2])
+}
+
+
+sectionPadre[1].insertBefore(parrafoNuevo, document.getElementById("btnVermas"))//agregar el nodo hijo
 }
